8020d46b3c2958f13fa10fe0d49d381775a95003
yuyang <yuyang@franknetwork.co>
2025-05-13 10:07:35 +0800

    initial commit

---
+++ android/app/src/main/kotlin/com/example/friday_ui/MainActivity.kt
@@ -0,0 +1,5 @@
+package com.example.friday_ui
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity : FlutterActivity()
---
+++ android/app/src/main/kotlin/com/friday/friday_ui/MainActivity.kt
@@ -0,0 +1,5 @@
+package com.friday.friday_ui
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity : FlutterActivity()
